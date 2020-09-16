5075b17a6669d58a2b87310878bdd48a61d9abaf
Popov Andrii <autoxarkov@gmail.com>
2020-09-17 00:53:53 +0300

    test1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,54 @@
-// ClientServerRandomData.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+#include <vector>
+
+HANDLE h = INVALID_HANDLE_VALUE;
+CRITICAL_SECTION   section;
+
+std::vector<int> data;
+
+DWORD WINAPI Client(LPVOID arg) {
+    int id = (int)arg;
+    
+    //EnterCriticalSection(&section);
+    DWORD res = WaitForSingleObject(h, INFINITE);
+    std::cout << id << " Client recived data " << data[0] << "\n";
+    //LeaveCriticalSection(&section);
+    return 0;
+}
+
+DWORD WINAPI Server(LPVOID arg) {
+    LONG prevVal = 0;
+    
+    EnterCriticalSection(&section);
+    data.push_back(10501);
+
+    for (int i = 0; i < 20; ++i) {
+        Sleep(3000);
+        std::cout << "Server second data \n";
+      ReleaseSemaphore(h, 1, &prevVal);
+    
+        data[0] = 10501 + i;
+    }
+    LeaveCriticalSection(&section);
+
+    return 0;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    h = CreateSemaphore(0, 0, 10, L"ServerDataSync");
+    
+    InitializeCriticalSection(&section);
+    CreateThread(0, 0, Server, 0, 0, 0);
+    
+
+    for (int i = 0; i < 20; ++i) {
+        CreateThread(0, 0, Client, (LPVOID)i, 0, 0);
+    }
+    DeleteCriticalSection(&section);
+
+    Sleep(50000);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
