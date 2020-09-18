b3f7a629fdbfbbf24c8592d55bc07235d7f7e989
Popov Andrii <autoxarkov@gmail.com>
2020-09-18 23:21:52 +0300

    worked fo 1 thred

--- main.cpp
+++ main.cpp
@@ -3,34 +3,43 @@
 #include <vector>
 
 HANDLE h = INVALID_HANDLE_VALUE;
-CRITICAL_SECTION   section;
+HANDLE ping = INVALID_HANDLE_VALUE;
 
 std::vector<int> data;
 
 DWORD WINAPI Client(LPVOID arg) {
     int id = (int)arg;
     
-    //EnterCriticalSection(&section);
     DWORD res = WaitForSingleObject(h, INFINITE);
     std::cout << id << " Client recived data " << data[0] << "\n";
-    //LeaveCriticalSection(&section);
+
+    HANDLE e = OpenEventA(EVENT_MODIFY_STATE, true, "Server");
+    if (e == INVALID_HANDLE_VALUE) return -1;
+    SetEvent(e);
+
     return 0;
 }
 
 DWORD WINAPI Server(LPVOID arg) {
     LONG prevVal = 0;
-    
-    EnterCriticalSection(&section);
-    data.push_back(10501);
 
-    for (int i = 0; i < 20; ++i) {
-        Sleep(3000);
+    data.push_back(0);
+
+    for (int i = 0; i < 6; ++i) {
+        Sleep(1000);
         std::cout << "Server second data \n";
-      ReleaseSemaphore(h, 1, &prevVal);
+        ReleaseSemaphore(h, 2, &prevVal);
     
-        data[0] = 10501 + i;
+        //for (int j = 0; j < 2; ++j) {
+
+            DWORD res = WaitForSingleObject(ping, INFINITE);
+            data[0] = rand() % 10;
+            DWORD res1 = WaitForSingleObject(ping, INFINITE);
+            data[0] = rand() % 10 + 1;
+
+        //}
+        std::cout << "UnSleepServer\n";
     }
-    LeaveCriticalSection(&section);
 
     return 0;
 }
@@ -39,16 +48,17 @@ DWORD WINAPI Server(LPVOID arg) {
 int main()
 {
 
-    h = CreateSemaphore(0, 0, 10, L"ServerDataSync");
-    
-    InitializeCriticalSection(&section);
-    CreateThread(0, 0, Server, 0, 0, 0);
-    
+    ping = CreateEventA(0, FALSE, FALSE, "Server");
+    if (ping == INVALID_HANDLE_VALUE) return -1;
 
-    for (int i = 0; i < 20; ++i) {
+    h = CreateSemaphore(0, 0, 10, L"ServerDataSync");
+    if (h == INVALID_HANDLE_VALUE) return -1;
+
+    CreateThread(0, 0, Server, 0, 0, 0);
+    Sleep(2000);
+    for (int i = 0; i < 6; ++i) {
         CreateThread(0, 0, Client, (LPVOID)i, 0, 0);
     }
-    DeleteCriticalSection(&section);
 
     Sleep(50000);
 }
