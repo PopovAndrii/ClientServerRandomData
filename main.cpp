ef1b96f86816628a4b5aa95885d501cdaff925e4
Popov Andreii <autoxarkov@gmail.com>
2020-09-18 07:54:36 -0700

    test

--- main.cpp
+++ main.cpp
@@ -3,34 +3,37 @@
 #include <vector>
 
 HANDLE h = INVALID_HANDLE_VALUE;
-CRITICAL_SECTION   section;
+HANDLE e = INVALID_HANDLE_VALUE;
 
 std::vector<int> data;
 
 DWORD WINAPI Client(LPVOID arg) {
     int id = (int)arg;
-    
-    //EnterCriticalSection(&section);
+
     DWORD res = WaitForSingleObject(h, INFINITE);
     std::cout << id << " Client recived data " << data[0] << "\n";
-    //LeaveCriticalSection(&section);
+    
+    e = OpenEventA(EVENT_MODIFY_STATE, true, "Server");
+    if (e == INVALID_HANDLE_VALUE) return -1;
+    SetEvent(e);
+
     return 0;
 }
 
 DWORD WINAPI Server(LPVOID arg) {
     LONG prevVal = 0;
     
-    EnterCriticalSection(&section);
-    data.push_back(10501);
+    data.push_back(0);
 
     for (int i = 0; i < 20; ++i) {
-        Sleep(3000);
+        Sleep(1000);
         std::cout << "Server second data \n";
-      ReleaseSemaphore(h, 1, &prevVal);
+        ReleaseSemaphore(h, 1, &prevVal);
     
         data[0] = 10501 + i;
+
+        DWORD res = WaitForSingleObject(e, INFINITE);
     }
-    LeaveCriticalSection(&section);
 
     return 0;
 }
@@ -40,15 +43,13 @@ int main()
 {
 
     h = CreateSemaphore(0, 0, 10, L"ServerDataSync");
-    
-    InitializeCriticalSection(&section);
+
     CreateThread(0, 0, Server, 0, 0, 0);
     
 
     for (int i = 0; i < 20; ++i) {
         CreateThread(0, 0, Client, (LPVOID)i, 0, 0);
     }
-    DeleteCriticalSection(&section);
 
     Sleep(50000);
 }
